eb18cb250020b36f2d5dcebe6e70dca7f91da1ed
danieljmann96 <djmann1996@gmail.com>
2022-04-12 01:35:55 +0100

    games won and total

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,13 @@
 import { ScoreKeys } from 'types';
 
-export function useLocalScore(scoreKey: ScoreKeys) {
+export function useLocalScore(
+  scoreKey: ScoreKeys
+): [string, (newScore: string) => void] {
   function updateScore(newScore: string) {
     localStorage.setItem(scoreKey, newScore);
   }
   if (localStorage.getItem(scoreKey) === null) {
     localStorage.setItem(scoreKey, '0');
   }
-  return [localStorage.getItem(scoreKey), updateScore];
+  return [localStorage[scoreKey], updateScore];
 }
