8867d9479ca0af4aedc2de538c40f49fa024c7e4
danieljmann96 <djmann1996@gmail.com>
2022-05-14 12:09:25 +0100

    lazy loading

--- src/components/LeftPane.tsx
+++ src/components/LeftPane.tsx
@@ -1,8 +1,8 @@
-import React, { SetStateAction, Dispatch } from 'react';
-import { Drawer, Box, List } from '@mui/material';
-import StatListItem from './StatListItem';
+import React, { SetStateAction, Dispatch, lazy, Suspense } from 'react';
+import { Drawer, Box, List, CircularProgress } from '@mui/material';
 import { useAllLocalScoreValues } from '../hooks';
-import ScoreBreakdownGraph from './ScoreBreakdownGraph';
+const StatListItem = lazy(() => import('./StatListItem'));
+const ScoreBreakdownGraph = lazy(() => import('./ScoreBreakdownGraph'));
 
 interface IProps {
   open: boolean;
@@ -12,32 +12,34 @@ interface IProps {
 export default function LeftPane({ open, setOpen }: IProps): JSX.Element {
   const allScores = useAllLocalScoreValues();
   return (
-    <Drawer anchor="left" onClose={() => setOpen(false)} open={open}>
-      <Box role="presentation" sx={{ width: 350 }}>
-        <List>
-          {[...allScores].map(scoreRecord => (
+    <Suspense fallback={<CircularProgress />}>
+      <Drawer anchor="left" onClose={() => setOpen(false)} open={open}>
+        <Box role="presentation" sx={{ width: 350 }}>
+          <List>
+            {[...allScores].map(scoreRecord => (
+              <StatListItem
+                key={scoreRecord[0]}
+                score={scoreRecord[1]}
+                scoreKey={scoreRecord[0]}
+              />
+            ))}
             <StatListItem
-              key={scoreRecord[0]}
-              score={scoreRecord[1]}
-              scoreKey={scoreRecord[0]}
+              score={
+                allScores.get('totalPlayed') === '0'
+                  ? '0'
+                  : `${Math.round(
+                      (Number(allScores.get('gamesWon')) /
+                        Number(allScores.get('totalPlayed'))) *
+                        100
+                    )}%`
+              }
+              scoreKey="Win Percentage"
+              shrinkIconText={allScores.get('totalPlayed') !== '0'}
             />
-          ))}
-          <StatListItem
-            score={
-              allScores.get('totalPlayed') === '0'
-                ? '0'
-                : `${Math.round(
-                    (Number(allScores.get('gamesWon')) /
-                      Number(allScores.get('totalPlayed'))) *
-                      100
-                  )}%`
-            }
-            scoreKey="Win Percentage"
-            shrinkIconText={allScores.get('totalPlayed') !== '0'}
-          />
-          <ScoreBreakdownGraph />
-        </List>
-      </Box>
-    </Drawer>
+            <ScoreBreakdownGraph />
+          </List>
+        </Box>
+      </Drawer>
+    </Suspense>
   );
 }
